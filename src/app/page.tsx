94d3998735b0a9f676f63b4af25ef23870780225
lucasmingatos <lucasmingatos@hotmail.com>
2024-04-24 11:29:27 -0300

    Commit final

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { StudentTable } from "./components/StudentTable";
+import { students } from "./data/students";
+
+const Page = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-5x1 mb-5">Lista de Estudantes</h1>
+      <StudentTable students = {students}/>
+    </div>
+  );
+}
+
+export default Page;
